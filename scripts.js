3d9f36daf6fd4e6419676984bc022f974879787f
Hanoon02 <ahmedhanoon02@gmail.com>
2022-08-16 12:39:55 +0400

    Basics

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Object array
+let myBooks = []
+
+
+// Object constructor
+
+function Book(title, author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return this.title + " by " + this.author + " " + this.pages + " pages " + this.read;
+    }
+}
+
+const book1 = Object.create(Book);
+book1.title = "The Hobbit";
+book1.author = "J.R.R. Tolkien";
+book1.pages = 310;
+book1.read = true;
+myBooks.push(book1);
+
+function addBook(){
+    
+}
