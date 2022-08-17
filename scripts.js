432c4b36ea3ae658401b86bab6851194f6ea9865
Hanoon02 <ahmedhanoon02@gmail.com>
2022-08-17 11:52:22 +0400

    addbook function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-// Object array
-let myBooks = []
-
 class Books{
     constructor(
         title = "blank",
@@ -28,26 +25,35 @@ class Library{
 const library = new Library();
 // Object constructor
 
-function Book(title, author,pages,read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return this.title + " by " + this.author + " " + this.pages + " pages " + this.read;
-    }
-}
+// function Book(title, author,pages,read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function(){
+//         return this.title + " by " + this.author + " " + this.pages + " pages " + this.read;
+//     }
+// }
 
-const book1 = Object.create(Book);
-book1.title = "The Hobbit";
-book1.author = "J.R.R. Tolkien";
-book1.pages = 310;
-book1.read = true;
-myBooks.push(book1);
+// const book1 = Object.create(Book);
+// book1.title = "The Hobbit";
+// book1.author = "J.R.R. Tolkien";
+// book1.pages = 310;
+// book1.read = true;
+// myBooks.push(book1);
 
-function addBook(){
-
-}
 
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addBook);
+
+const getBookInput = () =>{
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    return new Book(title, author, pages);
+}
+
+const addBook = () =>{
+    const newBook = getBookInput();
+    library.addBook(newBook);
+}
